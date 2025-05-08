7e3ec4f66fc8e878c8b9a2946a5c0a82e5716016
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2025-05-08 16:48:34 -0300

    refactor: use orchestrator.clearDatabase() in tests

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.clearDatabase();
 });
 
 describe("POST /api/v1/migrations", () => {
