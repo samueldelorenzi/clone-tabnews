88d81bbf04def810393bb061754f693d15d1ee89
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2025-05-14 16:01:55 -0300

    feat([): implement serviceerror in database.js

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,9 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\nErro dentro do catch do database");
-    console.error(error);
-    throw error;
+    const serviceErrorObj = new ServiceError({
+      message: "Erro na conexão com o banco ou na query",
+      cause: error,
+    });
+    throw serviceErrorObj;
   } finally {
     await client?.end();
   }
