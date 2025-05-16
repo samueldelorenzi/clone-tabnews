624c0752a113d9b920515aaf94004d7f5a2c1003
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2025-05-16 15:54:21 -0300

    feat: add ValidationError

--- infra/errors.js
+++ infra/errors.js
@@ -3,7 +3,7 @@ export class InternalServerError extends Error {
     super("Um erro interno não esperado aconteceu", {
       cause,
     });
-    this.name = "Internal Server Error";
+    this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte";
     this.statusCode = statusCode || 500;
   }
@@ -44,6 +44,26 @@ export class MethodNotAllowedError extends Error {
     this.name = "MethodNotAllowedError";
     this.action = "Verifique se o método HTTP é válido para este endpoint";
     this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ValidationError extends Error {
+  constructor({ cause, action, message }) {
+    super(message || "Um erro de validação ocorreu.", {
+      cause,
+    });
+    this.name = "ValidationError";
+    this.action = action || "Ajuste os dados enviados e tente novamente.";
+    this.statusCode = 400;
   }
 
   toJSON() {
