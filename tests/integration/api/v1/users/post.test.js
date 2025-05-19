2789841a5a572ded1c8ea6c0b24e1b32bfdabf95
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2025-05-19 11:15:55 -0300

    refactor: change describe to post /api/v1/users

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
   await orchestrator.runPendingMigrations();
 });
 
-describe("POST /api/v1/migrations", () => {
+describe("POST /api/v1/users", () => {
   describe("Anonymous user", () => {
     test("With unique and valid data", async () => {
       const response = await fetch("http://localhost:3000/api/v1/users", {
