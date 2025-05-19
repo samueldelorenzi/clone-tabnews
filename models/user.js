dae9b89cdb4728938602c8f953509dc9499ea07b
samueldelorenzi <samueldelorenziribeiro@gmail.com>
2025-05-19 11:22:43 -0300

    feat: add findOneByUsername function in user model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 import database from "infra/database";
-import { ValidationError } from "infra/errors";
+import { ValidationError, NotFoundError } from "infra/errors";
 
 async function create(userInputValues) {
   await validateUniqueEmail(userInputValues.email);
@@ -48,8 +48,30 @@ async function create(userInputValues) {
   }
 }
 
+async function findOneByUsername(username) {
+  const userFound = await runSelectQuery(username);
+  return userFound;
+
+  async function runSelectQuery(username) {
+    const results = await database.query({
+      text: "SELECT * FROM users WHERE LOWER(username) = LOWER($1) LIMIT 1;",
+      values: [username],
+    });
+
+    if (results.rowCount === 0) {
+      throw new NotFoundError({
+        message: "O username informado não foi encontrado no sistema.",
+        action: "Verifique se o username foi digitado corretamente",
+      });
+    }
+
+    return results.rows[0];
+  }
+}
+
 const user = {
   create,
+  findOneByUsername,
 };
 
 export default user;
